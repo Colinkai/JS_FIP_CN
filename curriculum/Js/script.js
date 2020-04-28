b01fa8744504979e076754424fa0e8e2f91d308c
463790370@qq.com <463790370@qq.com>
2020-04-29 01:09:50 +0800

    json数据传输

--- curriculum/Js/script.js
+++ curriculum/Js/script.js
@@ -10,21 +10,21 @@ const divDom = window.Colin.$("table-data-wrap");
 const tableDome = window.Colin.createEl("table"); //creatElement 创建dom对象
 tableDome.width = "100%"; //setAttribute 设置属性
 window.Colin.setAttr(tableDome, styles);
-tableDome.innerHTML = `<thead>
-                        <tr>
-                        <th>头像</th>
-                        <th>姓名</th>
-                        <th>性别</th>
-                        <th>年龄</th>
-                        <th>手机号</th>
-                        <th>国籍</th>
-                        <th width="200">爱好</th>
-                        <th>头衔</th>
-                        <th>操作</th>
-                        </tr>
-                      </thead>`;
+// tableDome.innerHTML = `<thead>
+//                         <tr>
+//                         <th>头像</th>
+//                         <th>姓名</th>
+//                         <th>性别</th>
+//                         <th>年龄</th>
+//                         <th>手机号</th>
+//                         <th>国籍</th>
+//                         <th width="200">爱好</th>
+//                         <th>头衔</th>
+//                         <th>操作</th>
+//                         </tr>
+//                       </thead>`;
 
-divDom.appendChild(tableDome);
+
 // console.log(tableDome);
 //array
 const tableTheadData = [{
@@ -56,8 +56,14 @@ const tableTheadData = [{
     label: "操作"
   },
 ];
+
 let theadHtml = `<thead><tr>`;
-tableTheadData.forEach(function (item, index, arr) {
-  theadHtml += `<th>${item}</th>`
-})
-theadHtml += `</tr></thead>`
+tableTheadData.forEach(function (item) {
+  //检测是否存在width
+  let width = item.width ? ` width="${item.width}"` : '';
+  theadHtml += `<th${width}>${item.label}</th>`
+});
+theadHtml += `</tr></thead>`;
+tableDome.innerHTML = theadHtml;
+console.log(theadHtml);
+divDom.appendChild(tableDome);
