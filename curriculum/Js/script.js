2fdf9ba2220df2f8d7512e26c8a6be346803d857
463790370@qq.com <463790370@qq.com>
2020-04-29 16:33:02 +0800

    假数据实验

--- curriculum/Js/script.js
+++ curriculum/Js/script.js
@@ -27,36 +27,6 @@ window.Colin.setAttr(tableDome, styles);
 
 // console.log(tableDome);
 //array
-const tableTheadData = [{
-    label: "头像"
-  },
-  {
-    label: "姓名"
-  },
-  {
-    label: "性别"
-  },
-  {
-    label: "年龄"
-  },
-  {
-    label: "手机号"
-  },
-  {
-    label: "国籍"
-  },
-  {
-    label: "爱好",
-    width: 200,
-  },
-  {
-    label: "头衔"
-  },
-  {
-    label: "操作"
-  },
-];
-
 let theadHtml = `<thead><tr>`;
 tableTheadData.forEach(function (item) {
   //检测是否存在width
@@ -64,6 +34,90 @@ tableTheadData.forEach(function (item) {
   theadHtml += `<th${width}>${item.label}</th>`
 });
 theadHtml += `</tr></thead>`;
-tableDome.innerHTML = theadHtml;
+/**
+ * 表格内容
+ */
+let tbodyHtml = `<tbody>`;
+let trHtml = ``;
+for (let i = 0; i < tableBodyData.length; i++) { //块级作用域,及大括号内，子级作用域，花括号外 父级作用域。
+  const data = tableBodyData[i];
+  //开始tr
+  trHtml += `<tr>`;
+  //头像
+  trHtml += `<td>
+                <div class="face">
+                    <span class="gender icon-${data.face.gender}"></span>
+                    <img src="${data.face.img}" alt="">
+                </div>
+              </td>`;
+  //姓名
+  trHtml += `<td>
+              <div class="people-name">
+                  <h4 class="name">${data.name.trueName}</h4>
+                  <span class="nickname option-05 fs-12">昵称：${data.name.nickname}</span>
+              </div>
+            </td>`;
+  //性别
+  trHtml += `<td>
+              <div class="gender-wrap">
+                  <span class="gender icon-girl ${data.face.gender==='girl'?'':'option-05'}"></span>
+                  <span class="gender icon-boy ${data.face.gender==='boy'?'':'option-05'}"></span>
+              </div>
+            </td>`;
+  //年龄
+  trHtml += `<td>
+  <div class="age text-center">
+      <p>${data.age.number}</p>
+      <span class="option-05 fs-12">（单身狗）</span>
+  </div>
+</td>`;
+  //手机号
+  trHtml += `<td>
+  <div class="phone">
+      ${data.phone.code}<span class="option-05">（中国）</span><br />
+      ${data.phone.numbe}
+  </div>
+</td>`;
+  //国籍
+  trHtml += `<td>
+              <div>
+                  <img src="${data.country.National_flag}" alt="${data.country.name}">
+                  <p class="country-name" style="color: ${data.country.color};">${data.country.name}</p>
+              </div>
+            </td>`;
+  //爱好
+  let likeHtml = ``;
+  data.like.forEach(item => {
+    likeHtml += `<span style="background-color: ${item.color};">${item.tag}</span>`
+  });
+  trHtml += `<td><div class="likes">${likeHtml}</div></td>`;
+  //头衔
+  trHtml += `<td>
+  <div class="grade">
+      <span class="role-name">${data.rank.name}</span>
+      <div class="grade-wrap icon-grade">
+          <div class="grade-high icon-grade" style="width: ${data.rank.lever*16}%;"></div>
+      </div>
+  </div>
+</td>`;
+  //操作
+  trHtml += `<td>
+  <div class="operation">
+      <a href="javascript: void(0);" class="operation-btn">设置</a>
+      <ul class="links">
+          <a href="javascript: void(0);">编辑</a>
+          <a href="javascript: void(0);">删除</a>
+          <a href="javascript: void(0);">锁定</a>
+          <a href="javascript: void(0);">收起</a>
+      </ul>
+  </div>
+</td>`;
+  //结束tr          
+  trHtml += `</tr>`;
+
+}
+tbodyHtml += trHtml;
+tbodyHtml += `</tbody>`;
+tableDome.innerHTML = theadHtml + tbodyHtml;
 console.log(theadHtml);
 divDom.appendChild(tableDome);
